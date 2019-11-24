4d4299d54235a593a22fca442a20c544afac65bf
Umit Turk <kumitturk@gmail.com>
2019-11-25 00:06:32 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+/*
+const name = 'umit';
+const age = 39;
+console.log(`My name is ${name} and I am ${age}`)
+*/
+
+//Arrays
+const numbers = new Array(1,2,3,4,5);
+console.log(numbers);
+
+const fruits = ['apples', 'oranges', 'pears'];
+
+
+fruits.push('mangos');
+fruits.unshift('strawberries');
+fruits.pop();
+
+console.log(fruits);
+
+console.log(window);
